f9500ef4fef7492d985ae6660e3200428b404b90
Xopppek <xopppekest@gmail.com>
2024-02-27 22:41:11 +0300

    added animation for analytical solution

--- lab1/main.py
+++ lab1/main.py
@@ -2,12 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-x0 = 4
-e = 3
-a = 0.5
+x0 = 1
+e = 2
+a = 3
 h = 0.01
 c = 0.67
 tau = c*h/a
+x_min = 0
+x_max = 30
+y_min = -0.1
+y_max = 1.1
+
+x = np.arange(x_min, x_max, h)
+t = np.arange(0, 5, tau)
 
 def xi(x):
     return np.abs(x-x0)/e
@@ -28,18 +35,22 @@ def mu(x):
 def analyt_sol(x, t):
     return phi(x-a*t)*np.heaviside(x-a*t, 1) + mu(t - x/a) * np.heaviside(a*t-x, 0)
 
+fig, axes = plt.subplots(2, 1, figsize=(12, 8))
+x_sol_axis, x_err_axis = axes[0], axes[1]
+x_sol_axis.set_xlabel('x')
+x_sol_axis.set_xlim(x_min, x_max)
+x_sol_axis.set_ylim(y_min, y_max)
+x_err_axis.set_xlabel('x')
+x_err_axis.set_xlim(x_min, x_max)
 
-x = np.arange(0, 20, h)
-t = np.arange(0, 5, tau)
-plt.plot(x, analyt_sol(x, 1))
-plt.show()
+analyt_line, = x_sol_axis.plot([], [], label='analytical')
 
-'''
-x = np.linspace(0, 10, 100)
-plt.plot(x, phi1(x), label='1')
-plt.plot(x, phi2(x), label='2')
-plt.plot(x, phi3(x), label='3')
-plt.plot(x, phi4(x), label='4')
-plt.legend()
+def update(t):
+    curr_analyt = analyt_sol(x, t)
+    analyt_line.set_data(x, curr_analyt)
+    return analyt_line
+
+anim = FuncAnimation(fig, update, frames=t, init_func=None, interval=5)
+
+x_sol_axis.legend()
 plt.show()
-'''
