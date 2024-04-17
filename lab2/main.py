5a64a1937bd06306b58b19ce1bcdfea89ac76336
Xopppek <xopppekest@gmail.com>
2024-04-17 21:48:46 +0300

    implemented lab2

--- lab2/main.py
+++ lab2/main.py
@@ -15,7 +15,7 @@ x_min = -l
 x_max = l
 y_min = -0.1
 y_max = 1.45
-t_end = 10
+t_end = 8
 
 def speed(x):
     return -a*np.tanh(x/l)
@@ -40,11 +40,12 @@ def phi4(x):
 def phi(x):
     return phi4(x)
 def mu_l(t):
-    return np.sin(4*t)
-    #return 0
+    #return np.sin(4*t)
+    return phi1(t)
+    return 0
 def mu_r(t):
-    return -np.cos(t*5)
-    #return 0
+    #return np.sin(t*4)
+    return 0
 
 def analyt_sol(x, t : float):
     x_temp = l * np.arcsinh(np.sinh(1)*np.exp(-a*t/l))
@@ -68,12 +69,13 @@ x_err_axis.set_ylim(0, 1)
 
 analyt_line, = x_sol_axis.plot([], [], label = 'аналитическое решение', color='green')
 
-num_line_h1, =  x_sol_axis.plot([], [], label = f'уголок h={h1}', color='orange')
-num_line_h2, =  x_sol_axis.plot([], [], label = f'уголок h={h2}', color='dodgerblue')
+num_line_h1, =  x_sol_axis.plot([], [], label = f'Л-В h={h1}', color='orange')
+num_line_h2, =  x_sol_axis.plot([], [], label = f'Л-В h={h2}', color='dodgerblue')
 err_h1, = x_err_axis.plot([], [], label = f'h={h1}', color='orange')
 err_h2, = x_err_axis.plot([], [], label = f'h={h2}', color='dodgerblue')
 
 def update(t):
+    zerostate = phi(0)
     analyt_line.set_data(x_h2, analyt_sol(x_h2, t))
     if (t == 0):
         num_line_h1.set_data(x_h1, phi(x_h1))
@@ -87,40 +89,49 @@ def update(t):
         err_temp = 0
         for i in range(10):
             h1_temp = np.zeros_like(x_h1)
-            z = prev_step_h1[l][1:] - speed(x_h1[l][1:]) * tau(h1)/h1 * (prev_step_h1[l][1:] - prev_step_h1[l][:-1])
-            h1_temp[l] = np.concatenate(([0], z))
-            z = prev_step_h1[r][:-1] - speed(x_h1[r][:-1]) * tau(h1)/h1 * (prev_step_h1[r][1:] - prev_step_h1[r][:-1])
-            h1_temp[r] = np.concatenate((z, [0]))
             h1_temp[0] = mu_l(t+i*tau(h1))
             h1_temp[-1] = mu_r(t+i*tau(h1))
+            z =  prev_step_h1[l][1:-1] - tau(h1) * speed(x_h1[l][1:-1])/(2*h1) * (prev_step_h1[l][2:] - prev_step_h1[l][:-2]) + \
+                tau(h1)**2 * speed(x_h1[l][1:-1])/(2*h1**2) * (  speed(x_h1[l][1:-1] + h1/2) * (prev_step_h1[l][2:]-prev_step_h1[l][1:-1]) -\
+                                                               speed(x_h1[l][1:-1]-h1/2)* (prev_step_h1[l][1:-1] - prev_step_h1[l][:-2]) )
+            h1_temp[l] = np.concatenate(([mu_l(t+i*tau(h1))], z, [zerostate]))
+            z = prev_step_h1[r][1:-1] - tau(h1) * speed(x_h1[r][1:-1])/(2*h1) * (prev_step_h1[r][2:] - prev_step_h1[r][:-2]) + \
+                tau(h1)**2 * speed(x_h1[r][1:-1])/(2*h1**2) * (  speed(x_h1[r][1:-1] + h1/2) * (prev_step_h1[r][2:]-prev_step_h1[r][1:-1]) -\
+                                                               speed(x_h1[r][1:-1]-h1/2)* (prev_step_h1[r][1:-1] - prev_step_h1[r][:-2]) )
+            h1_temp[r] = np.concatenate(([zerostate], z, [mu_r(t+i*tau(h1))]))
             prev_step_h1 = h1_temp
             err_temp = np.max(np.abs(analyt_sol(x_h1, t) - h1_temp))
         num_line_h1.set_data(x_h1, h1_temp)
         err_h1.set_data(np.concatenate((err_h1.get_xdata(), [t])), np.concatenate((err_h1.get_ydata(), [err_temp])))
         #print(err_h1.get_ydata())
         #print(np.abs(analyt_sol(x_h1, t) - h1_temp)[1500:-1000])
+        
         l = x_h2 < 0
         r = x_h2 >= 0
         prev_step_h2 = num_line_h2.get_ydata() 
         for i in range(100):
             h2_temp = np.zeros_like(x_h2)
-            z = prev_step_h2[l][1:] - speed(x_h2[l][1:]) * tau(h2)/h2 * (prev_step_h2[l][1:] - prev_step_h2[l][:-1])
-            h2_temp[l] = np.concatenate(([0], z))
-            z = prev_step_h2[r][:-1] - speed(x_h2[r][:-1]) * tau(h2)/h2 * (prev_step_h2[r][1:] - prev_step_h2[r][:-1])
-            h2_temp[r] = np.concatenate((z, [0]))
             h2_temp[0] = mu_l(t+i*tau(h2))
             h2_temp[-1] = mu_r(t+i*tau(h2))
+            z =  prev_step_h2[l][1:-1] - tau(h2) * speed(x_h2[l][1:-1])/(2*h2) * (prev_step_h2[l][2:] - prev_step_h2[l][:-2]) + \
+                tau(h2)**2 * speed(x_h2[l][1:-1])/(2*h2**2) * (  speed(x_h2[l][1:-1] + h2/2) * (prev_step_h2[l][2:]-prev_step_h2[l][1:-1]) -\
+                                                               speed(x_h2[l][1:-1]-h2/2)* (prev_step_h2[l][1:-1] - prev_step_h2[l][:-2]) )
+            h2_temp[l] = np.concatenate(([mu_l(t+i*tau(h2))], z, [zerostate]))
+            z = prev_step_h2[r][1:-1] - tau(h2) * speed(x_h2[r][1:-1])/(2*h2) * (prev_step_h2[r][2:] - prev_step_h2[r][:-2]) + \
+                tau(h2)**2 * speed(x_h2[r][1:-1])/(2*h2**2) * (  speed(x_h2[r][1:-1] + h2/2) * (prev_step_h2[r][2:]-prev_step_h2[r][1:-1]) -\
+                                                               speed(x_h2[r][1:-1]-h2/2)* (prev_step_h2[r][1:-1] - prev_step_h2[r][:-2]) )
+            h2_temp[r] = np.concatenate(([zerostate], z, [mu_r(t+i*tau(h2))]))
             prev_step_h2 = h2_temp
             err_temp = np.max(np.abs(analyt_sol(x_h2, t) - h2_temp))
         num_line_h2.set_data(x_h2, h2_temp)
         err_h2.set_data(np.concatenate((err_h2.get_xdata(), [t])), np.concatenate((err_h2.get_ydata(), [err_temp])))
+        
 
     x_sol_axis.set_title(f'time = {t:.2f}')
     x_err_axis.set_title(r"""$\epsilon_1/\epsilon_2$ =""" + f'{err_h1.get_ydata()[-1]/(err_h2.get_ydata()[-1] + 0.0001):.2f}')
-
-anim = FuncAnimation(fig, update, frames=animation_t, init_func=None, interval=1)
+anim = FuncAnimation(fig, update, frames=animation_t, init_func=None, interval=2)
 
 x_sol_axis.legend()
 x_err_axis.legend()
-#anim.save('analitical.gif')
+#anim.save('wtf3.gif')
 plt.show()
